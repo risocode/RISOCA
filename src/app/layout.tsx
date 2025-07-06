d55be2df1cc3e87262622e1974ea8e12b5589fb1
Risoca Developer <risoca.developer@gmail.com>
2025-07-06 07:23:16 +0000

    in the dashboard when in mobile. can you please fix up everything on the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark h-full">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <meta name="application-name" content="RiSoCa Bot" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
