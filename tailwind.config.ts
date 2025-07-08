c810c715a66c897acf9e329ab69510cb718e06d4
Risoca Developer <risoca.developer@gmail.com>
2025-07-08 19:15:18 +0000

    make the delay little bit smooth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,15 +76,15 @@ export default {
           '100%': {opacity: '1', transform: 'scale(1)'},
         },
         'page-fade-in': {
-          from: {opacity: '0'},
-          to: {opacity: '1'},
+          from: {opacity: '0', transform: 'translateY(8px)'},
+          to: {opacity: '1', transform: 'translateY(0)'},
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         enter: 'fade-in-scale-up 0.3s ease-out forwards',
-        'page-enter': 'page-fade-in 0.3s ease-out 0.3s forwards',
+        'page-enter': 'page-fade-in 0.5s ease-in-out forwards',
       },
     },
   },
